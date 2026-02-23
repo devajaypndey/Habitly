7bd8765c8e9dde8900a96833921d9cd610e18741
Ajay Pandey <ajaypandeyy1123@gmail.com>
2026-02-23 21:31:50 +0530

    Implementing Darksystem ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AppRoutes from "./routes/AppRoutes";
+import { useAppSelector } from "./app/hooks";
+import { useEffect } from "react";
 
 function App() {
+
+   const theme = useAppSelector((state) => state.ui.theme);
+
+   useEffect(() =>{
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+    root.classList.add(theme);
+   }, [theme]);
+
   return (
     <>
       <ToastContainer
