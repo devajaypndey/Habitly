23ea2921c314318b6513c977a6f3b9d91449d56d
Ajay Pandey <ajaypandeyy1123@gmail.com>
2026-06-13 02:52:54 +0530

    add new logo

--- vite.config.js
+++ vite.config.js
@@ -28,12 +28,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "logo192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "pwa-512x512.png",
+            src: "logo512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
